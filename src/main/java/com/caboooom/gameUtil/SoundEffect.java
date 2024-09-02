c56fc62aa0dc4853fde7194a2e0962273e933b2e
sylim <sammys27@naver.com>
2024-09-02 11:12:46 +0900

    효과음 재생 에러 해결
    Resolves: #8

--- src/main/java/com/caboooom/gameUtil/SoundEffect.java
+++ src/main/java/com/caboooom/gameUtil/SoundEffect.java
@@ -2,9 +2,6 @@ package com.caboooom.gameUtil;
 
 import javazoom.jl.decoder.JavaLayerException;
 import javazoom.jl.player.Player;
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
@@ -12,25 +9,19 @@ import java.io.IOException;
 public class SoundEffect {
 
     private final String filePath;
-    private static final Logger logger = LogManager.getLogger();
 
     public SoundEffect(String filePath) {
         this.filePath = filePath;
     }
 
     public void play() {
-        try (BufferedInputStream bufferedStream = new BufferedInputStream(getClass().getResourceAsStream(filePath));) {
-            Player player = new Player(bufferedStream);
-            new Thread(() -> {
-                try {
-                    player.play();
-                } catch (JavaLayerException e) {
-                    logger.log(Level.ERROR, "Failed to play sound effect: ", filePath);
-                    throw new RuntimeException(e);
-                }
-            }).start();
-        } catch (JavaLayerException | IOException e) {
-            throw new RuntimeException(e);
-        }
+        new Thread(() ->{
+            try (BufferedInputStream bufferedStream = new BufferedInputStream(getClass().getResourceAsStream(filePath));) {
+                Player player = new Player(bufferedStream);
+                player.play();
+            } catch (JavaLayerException | IOException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
     }
 }
--- src/main/java/com/caboooom/gameUtil/SoundEffectPlayer.java
+++ src/main/java/com/caboooom/gameUtil/SoundEffectPlayer.java
@@ -2,10 +2,10 @@ package com.caboooom.gameUtil;
 
 public class SoundEffectPlayer {
 
-    private static final SoundEffect ballHitSound = new SoundEffect("/sounds/ball-bounce.mp3");
-    private static final SoundEffect brickBreakSound = new SoundEffect("/sounds/brick-break.mp3");
-    private static final SoundEffect gameCompleteSound = new SoundEffect("/sounds/game-complete.mp3");
-    private static final SoundEffect gameOverSound = new SoundEffect("/sounds/game-over.mp3");
+    private static SoundEffect ballHitSound = new SoundEffect("/sounds/ball-bounce.mp3");
+    private static SoundEffect brickBreakSound = new SoundEffect("/sounds/brick-break.mp3");
+    private static SoundEffect gameCompleteSound = new SoundEffect("/sounds/game-complete.mp3");
+    private static SoundEffect gameOverSound = new SoundEffect("/sounds/game-over.mp3");
 
     public static void ballHit() {
         ballHitSound.play();
@@ -19,8 +19,7 @@ public class SoundEffectPlayer {
         gameCompleteSound.play();
     }
 
-    public static void gameOver() {
-        gameOverSound.play();
+    public static void gameOver() { gameOverSound.play();
     }
 
 }
